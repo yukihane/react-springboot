54050c0b2de3e2f8753f68a23de9a74273032488
yukihane <yukihane.feather@gmail.com>
2019-06-17 07:03:06 +0900

    book sources

--- cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -1,33 +1,39 @@
 package com.packt.cardatabase;
 
-import com.packt.cardatabase.domain.Car;
-import com.packt.cardatabase.domain.CarRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@RequiredArgsConstructor
+import com.packt.cardatabase.domain.Car;
+import com.packt.cardatabase.domain.CarRepository;
+import com.packt.cardatabase.domain.Owner;
+import com.packt.cardatabase.domain.OwnerRepository;
+
 @SpringBootApplication
 public class CardatabaseApplication {
+	@Autowired	
+	private CarRepository repository;
 
-    private final CarRepository repository;
-
-    public static void main(final String[] args) {
-        SpringApplication.run(CardatabaseApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner runner() {
-        return args -> {
-            // Save demo data to database
-            repository.save(new Car("Ford", "Mustang", "Red",
-                "ADF-1121", 2017, 59000));
-            repository.save(new Car("Nissan", "Leaf", "White",
-                "SSJ-3002", 2014, 29000));
-            repository.save(new Car("Toyota", "Prius", "Silver",
-                "KKO-0212", 2018, 39000));
-        };
-    }
+	@Autowired	
+	private OwnerRepository orepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CardatabaseApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			Owner owner1 = new Owner("John" , "Johnson");
+			Owner owner2 = new Owner("Mary" , "Robinson");
+			orepository.save(owner1);
+			orepository.save(owner2);
+			
+			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
+			repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));
+			repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));
+		};
+	}	
 }
